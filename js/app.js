e119bf40e93cbf12de9d70085b9b018c3f137ec9
Dima Zabrodin <zabrodin17081990@gmail.com>
2020-01-29 09:02:02 +0200

    show pie chart

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 var App = {
     ranges: {
-        in: {id: "#inhale-range", min: 1, max: 30, step: 1, infinity: false},
-        inHold: {id: "#inhale-hold-range", min: 1, max: 30, step: 1, infinity: false},
-        out: {id: "#exhale-range", min: 1, max: 30, step: 1, infinity: false},
-        outHold: {id: "#exhale-hold-range", min: 1, max: 30, step: 1, infinity: false},
-        duration: {id: "#duration-range", min: 1, max: 30, step: 1, infinity: true}
+        in: {id: "#inhale-range", name: 'In', min: 1, max: 30, step: 1, infinity: false, borderColor: 'tomato', backColor: 'white'},
+        inHold: {id: "#inhale-hold-range", name: "In\nHold", min: 1, max: 30, step: 1, infinity: false, borderColor: 'cyan', backColor: 'white'},
+        out: {id: "#exhale-range", name: "Out", min: 1, max: 30, step: 1, infinity: false, borderColor: 'blue', backColor: 'white'},
+        outHold: {id: "#exhale-hold-range", name: "Out\nHold", min: 1, max: 30, step: 1, infinity: false, borderColor: 'green', backColor: 'white'},
+        duration: {id: "#duration-range", min: 1, max: 30, step: 1, infinity: true, borderColor: 'green', backColor: 'white'}
     },
     getHandleText: function (range, value) {
         return range.infinity && range.max == value ? '∞' : value;
@@ -15,18 +15,36 @@ var App = {
     },
     tick: function() {
         var time = (Date.now() - App.startTime) / 1000;
-        $('#timeline').text(Math.floor(time));
+        // $('#timeline').text(Math.floor(time));
         var currentCycleDuration = (time % App.cycleDuration);
         var breathRanges = JSON.parse(JSON.stringify(App.ranges));
         delete(breathRanges.duration);
         $.each(breathRanges, function(rangeName, range) {
             if (range.value > currentCycleDuration) {
-                $('#currentAction').text(rangeName + ' ' + Math.round(currentCycleDuration / range.value * 100) + '%');
+                // $('#currentAction').text(rangeName + ' ' + Math.round(currentCycleDuration / range.value * 100) + '%');
+                App.drawCircle(range, currentCycleDuration / range.value);
                 return false;
             } else {
                 currentCycleDuration -= range.value;
             }
         });
+    },
+    drawCircle: function(range, percentage) {
+        const fullPerimetr = 510;
+        var svg = '<svg height="200" width="200" viewBox="0 0 200 200" style="transform: rotate(-0.25turn)">'+
+                    '<g id="circles">'+
+                        '<circle r="100" cx="100" cy="100" fill="' + range.backColor + '" />'+
+                        '<circle r="80" cx="100" cy="100" fill="' + range.backColor + '"'+
+                            ' stroke="' + range.borderColor + '"'+
+                            ' stroke-width="40"'+
+                            ' scale="2.0"' +
+                            ' stroke-dasharray="' + fullPerimetr * percentage + ' ' + fullPerimetr + '" />'+
+                            '<text x="50%"" y="50%" text-anchor="middle" transform="rotate(90 100,100)"  font-family="Arial, Helvetica, sans-serif" font-size="25px" dy=".3em">'+
+                            range.name +
+                            '</text>' +
+                    '</g>' +
+                    '</svg>';
+        $('#svg').html(svg);
     },
     calculateCycleDuration: function() {
         App.cycleDuration = 
@@ -65,7 +83,7 @@ var App = {
             clearInterval(App.interval);
             App.calculateCycleDuration();
             App.startTime = Date.now();
-            App.interval = setInterval(App.tick, 100);
+            App.interval = setInterval(App.tick, 40);
             App.tick();
         });
         $('#reset-counter').click(function() {
@@ -73,6 +91,7 @@ var App = {
             clearInterval(App.interval);
             $('#timeline').text('00:00:00');
             $('#currentAction').text('');
+            $('#svg').html('');
         });
     },
 };
